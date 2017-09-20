8b818d248627e8c5b6a79e640a1a4abc93531d0c
Chris Nixon <chris.d.nixon@gmail.com>
2017-09-19 22:56:39 -0700

    Don't prepend fingerprint, remove flexi stuff

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,7 +7,6 @@ module.exports = function(defaults) {
       clientAllowedKeys: ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY']
     },
     fingerprint: {
-      prepend: 'https://d6kj2kjnxtaea.cloudfront.net/',
       generateAssetMap: true,
       fingerprintAssetMap: true
     }
